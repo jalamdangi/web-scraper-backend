3b2feee0db7e169cfc2572ebff355b3b9a26c3eb
jalam <jalamdangi96@gmail.com>
2024-12-13 00:19:06 +0530

    updated file name

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/screenshots", express.static(path.join(__dirname, "screenshots")));
 
 // Routes
-app.get("/", (req,res)=>res.send("welcome to web scraper server !"))
+app.get("/", (req, res) => res.send("Welcome to the web scraper server!"));
 app.use("/api/companies", companyRoutes);
 
-// Start server
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel to use
+module.exports = app;
