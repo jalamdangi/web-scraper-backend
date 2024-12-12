94dbba52166f038b2f1494ff9e3636be767a16b8
jalam <jalamdangi96@gmail.com>
2024-12-13 02:15:14 +0530

    new db uri added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/web-scraper", {});
+    await mongoose.connect("mongodb+srv://jalamdangi96:59fARtmrbviA86g8@cluster0.hlvru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/web-scraper", {});
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
@@ -11,3 +11,9 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// jalamdangi96 
+// 59fARtmrbviA86g8
+
+// mongodb+srv://jalamdangi96:59fARtmrbviA86g8@cluster0.hlvru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// mongodb+srv://jalamdangi96:59fARtmrbviA86g8@cluster0.hlvru.mongodb.net/
