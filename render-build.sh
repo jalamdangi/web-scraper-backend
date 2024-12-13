a14fd9072aa20e514e9939e205e963854ad2b5e8
jalam <jalamdangi96@gmail.com>
2024-12-13 11:38:05 +0530

    Fix Puppeteer setup for Render

---
+++ render-build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Update package lists
+apt-get update
+
+# Install Chrome
+apt-get install -y wget unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install Puppeteer dependencies
+apt-get install -y \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libcups2 \
+  libdrm2 \
+  libgbm1 \
+  libnspr4 \
+  libnss3 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils
